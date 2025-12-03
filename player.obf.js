a96f99c5bbcc09527e5d8abf845e31546f988151
PodcastsDDP <sonianafa@gmail.com>
2025-12-03 13:42:19 -0300

    Update player.obf.js

--- player.obf.js
+++ player.obf.js
@@ -47,7 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
       audio: "https://duraznodigitalpremium.com/wp-content/uploads/2025/12/HolaDurazno2-3-diciembre.mp3"
     },
     {
-      title: "Navidad a Cielo Abierto",
+      title: "Navidad a Cielo Abierto en Durazno",
       description: "Otros datos de la conferencia de prensa",
       duration: "16:00",
       audio: "https://duraznodigitalpremium.com/wp-content/uploads/2025/12/Acieloabiertoconferencia.mp3"
@@ -149,3 +149,4 @@ document.addEventListener("DOMContentLoaded", () => {
   // ======= Init =======
   renderPlaylist();
 });
+
